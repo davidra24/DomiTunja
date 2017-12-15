a6fd876647ab9193cc83bd60651aadeb571d7e94
martinV97 <martin.vn11@gmail.com>
2017-12-14 23:09:32 -0500

    Splash Screen Completa

--- app/src/main/java/domitunja/appsolution/co/domitunja/SplashActivity.kt
+++ app/src/main/java/domitunja/appsolution/co/domitunja/SplashActivity.kt
@@ -1,12 +1,18 @@
 package domitunja.appsolution.co.domitunja
 
+import android.content.Intent
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.os.Handler
 
 class SplashActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.splash_screen)
+        Handler().postDelayed({
+                startActivity(Intent(this, MenuActivity::class.java))
+                finish()
+        }, 1500)
     }
 }
