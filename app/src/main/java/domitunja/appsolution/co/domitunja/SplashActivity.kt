e604080ec2a342cb103e7737602c198ef6c788ff
martinV97 <martin.vn11@gmail.com>
2017-12-14 19:02:30 -0500

    Cambios en Nombres

--- app/src/main/java/domitunja/appsolution/co/domitunja/SplashActivity.kt
+++ app/src/main/java/domitunja/appsolution/co/domitunja/SplashActivity.kt
@@ -3,10 +3,10 @@ package domitunja.appsolution.co.domitunja
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 
-class MainActivity : AppCompatActivity() {
+class SplashActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(R.layout.splash_screen)
     }
 }
