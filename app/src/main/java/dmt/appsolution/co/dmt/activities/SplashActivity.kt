1d769850579b7c95c479bca33b7bfc07c91ff3af
martinV97 <martin.vn11@gmail.com>
2017-12-18 20:29:21 -0500

    Adicion de Mapa en Pestaña Favoritos

--- app/src/main/java/dmt/appsolution/co/dmt/activities/SplashActivity.kt
+++ app/src/main/java/dmt/appsolution/co/dmt/activities/SplashActivity.kt
@@ -1,12 +1,17 @@
 package dmt.appsolution.co.dmt.activities
 
 import android.content.Intent
+import android.content.pm.PackageManager
+import android.os.Build
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 import android.os.Handler
+import android.support.annotation.RequiresApi
+import android.support.v4.content.ContextCompat
 import dmt.appsolution.co.dmt.R
 
 class SplashActivity : AppCompatActivity() {
+    private val REQUEST_CODE_ASK_PERMISSIONS = 123
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -15,5 +20,15 @@ class SplashActivity : AppCompatActivity() {
                 startActivity(Intent(this, MenuActivity::class.java))
                 finish()
         }, 1500)
+    }
+
+    @RequiresApi(Build.VERSION_CODES.M)
+    override fun onStart() {
+        super.onStart()
+        val hasWriteContactsPermission = ContextCompat.checkSelfPermission(this.applicationContext, "android.permission.ACCESS_FINE_LOCATION")
+        if (hasWriteContactsPermission != PackageManager.PERMISSION_GRANTED) {
+            requestPermissions(arrayOf("android.permission.ACCESS_FINE_LOCATION"),
+                    REQUEST_CODE_ASK_PERMISSIONS)
+        }
     }
 }
--- app/src/main/java/dmt/appsolution/co/dmt/fragments/FavoriteFragment.kt
+++ app/src/main/java/dmt/appsolution/co/dmt/fragments/FavoriteFragment.kt
@@ -1,27 +1,60 @@
 package dmt.appsolution.co.dmt.fragments
 
 
+import android.annotation.SuppressLint
 import android.os.Bundle
 import android.support.v4.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.Toast
+import com.google.android.gms.maps.GoogleMap
+import com.google.android.gms.maps.OnMapReadyCallback
 import dmt.appsolution.co.dmt.R
 import kotlinx.android.synthetic.main.fragment_favorite.*
-
 
 /**
  * A simple [Fragment] subclass.
  */
-class FavoriteFragment : Fragment() {
+class FavoriteFragment : Fragment(), OnMapReadyCallback {
 
     override fun onCreateView(inflater: LayoutInflater?, container: ViewGroup?,
+
                               savedInstanceState: Bundle?): View? {
         return inflater!!.inflate(R.layout.fragment_favorite, container, false)
     }
 
+    @SuppressLint("MissingPermission")
     override fun onActivityCreated(savedInstanceState: Bundle?) {
         super.onActivityCreated(savedInstanceState)
+        mapViewFavorite.onCreate(savedInstanceState)
+        mapViewFavorite.getMapAsync(this)
+    }
+
+
+
+    @SuppressLint("MissingPermission")
+    override fun onMapReady(map: GoogleMap?) {
+        //map!!.mapType = GoogleMap.MAP_TYPE_HYBRID
+        map!!.isMyLocationEnabled = true
+        //map.isTrafficEnabled = true
+        //map.isIndoorEnabled = true
+        //map.isBuildingsEnabled = true
+        map.uiSettings.isZoomControlsEnabled = true
+    }
+
+
+    override fun onResume() {
+        mapViewFavorite.onResume()
+        super.onResume()
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        mapViewFavorite.onDestroy()
+    }
+
+    override fun onLowMemory() {
+        super.onLowMemory()
+        mapViewFavorite.onLowMemory()
     }
 }
