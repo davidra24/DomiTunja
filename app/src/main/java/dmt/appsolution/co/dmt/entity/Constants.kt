1ce69f693c2e19aebcf708c576a99b2f0cefb4a8
Daniel Felipe Contreras López <cldfrkr@hotmail.com>
2018-01-09 22:19:08 -0500

    fragment more coplete

--- app/src/main/java/dmt/appsolution/co/dmt/entity/Constants.kt
+++ app/src/main/java/dmt/appsolution/co/dmt/entity/Constants.kt
@@ -11,6 +11,8 @@ class Constants {
         val MEAT_FOOD: String = "Carne"
         val FISH_FOOD: String = "Pescado"
         val DEFAULT_PHONE_NUMBER: String = "3191231234"
+        val FACEBOOK_URL: String = "https://www.facebook.com/appsolutionco"
+        val FACEBOOK_PAGE_ID: String = "https://www.facebook.com/appsolutionco"
         var restaurantList: MutableList<ItemRestaurant> = mutableListOf()
         var filterRestaurantList: MutableList<ItemRestaurant> = mutableListOf()
         var recommendedRestaurantList: MutableList<ItemRestaurant> = mutableListOf()
--- app/src/main/java/dmt/appsolution/co/dmt/fragments/MoreFragment.kt
+++ app/src/main/java/dmt/appsolution/co/dmt/fragments/MoreFragment.kt
@@ -10,10 +10,14 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import dmt.appsolution.co.dmt.R
-import dmt.appsolution.co.dmt.activities.MenuActivity
+import dmt.appsolution.co.dmt.entity.Constants
 import kotlinx.android.synthetic.main.fragment_more.*
+import android.content.pm.PackageManager
+
+
 
 class MoreFragment : Fragment() {
+
 
 
     override fun onCreateView(inflater: LayoutInflater?, container: ViewGroup?,
@@ -42,18 +46,31 @@ class MoreFragment : Fragment() {
 
     }
 
-    private fun openFacebook(context:Context):Intent{
+    /*private fun openFacebook(context:Context):Intent{
         try {
-            context.packageManager.getPackageInfo("com.facebook.katana", 0)
             return Intent(Intent.ACTION_VIEW, Uri.parse("fb://page/322033857845479"))
         }catch (e:Exception){
-            return Intent(Intent.ACTION_VIEW, Uri.parse("https://www.facebook.com/appsolutionco/"))
+            return Intent(Intent.ACTION_VIEW, Uri.parse(Constants.FACEBOOK_URL))
         }
+    }*/
+
+    private fun openFacebook(context:Context): Intent {
+        var uri = Uri.parse(Constants.FACEBOOK_URL)
+        var packManager = context.packageManager
+        try {
+            val applicationInfo =  packManager.getPackageInfo("com.facebook.katana", 0)
+            if (applicationInfo.versionCode >= 3002850) {
+                uri = Uri.parse("fb://facewebmodal/f?href=" + Constants.FACEBOOK_URL)
+            }
+        } catch (ignored: PackageManager.NameNotFoundException) {
+        }
+
+        return Intent(Intent.ACTION_VIEW, uri)
     }
 
     private fun openTwitter(context:Context):Intent{
         try {
-            context.packageManager.getPackageInfo("com.twitter.android", 0)
+            //context.packageManager.getPackageInfo("com.twitter.android", 0)
             return Intent(Intent.ACTION_VIEW, Uri.parse("twitter://user?user_id=519786756"))
         } catch (e: Exception) {
             return Intent(Intent.ACTION_VIEW, Uri.parse("https://twitter.com/appsolutionco"))
